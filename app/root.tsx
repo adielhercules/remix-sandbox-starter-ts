d8de00bcef3b1dcc7b8989b12cf0d9d4ad9e5b36
Adiel Hercules <jadher.11x2@gmail.com>
2022-01-13 00:11:49 +0100

    Add router

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,94 @@
 import {
-  Links,
-  LiveReload,
   Meta,
-  Outlet,
+  Links,
   Scripts,
-  ScrollRestoration
+  useLoaderData,
+  LiveReload,
+  useCatch,
+  LinksFunction,
 } from "remix";
-import type { MetaFunction } from "remix";
+import { Outlet } from "react-router-dom";
 
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+import stylesUrl from "./styles/global.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export default function App() {
+export function loader() {
+  return { date: new Date() };
+}
+
+function Document({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
+        {children}
         <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  let data = useLoaderData();
+
+  return (
+    <Document>
+      <Outlet />
+      <footer>
+        <p>This page was rendered at {data.date.toLocaleString()}</p>
+      </footer>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  let caught = useCatch();
+
+  switch (caught.status) {
+    case 401:
+    case 404:
+      return (
+        <Document title={`${caught.status} ${caught.statusText}`}>
+          <h1>
+            {caught.status} {caught.statusText}
+          </h1>
+        </Document>
+      );
+
+    default:
+      throw new Error(
+        `Unexpected caught response with status: ${caught.status}`
+      );
+  }
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
+  return (
+    <Document title="Uh-oh!">
+      <h1>App Error</h1>
+      <pre>{error.message}</pre>
+      <p>
+        Replace this UI with what you want users to see when your app throws
+        uncaught errors.
+      </p>
+    </Document>
   );
 }
--- app/routes/page2.tsx
+++ app/routes/page2.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Page 2</h1>
       <ul>
         <li>
-          <Link to="/page2">/page2</Link>
+          <Link to="/">Go back home</Link>
         </li>
       </ul>
     </div>
